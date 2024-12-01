c8721bc7f0c5aaf329ced503e25ba6081c9eb403
Rouf <roufshoukath@gmail.com>
2024-12-01 20:53:58 +0530

    jawharat 1.12 24

---
+++ js/function.js
@@ -0,0 +1,10 @@
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".navbar ul a");
+const menuLength = menuItem.length;
+
+
+for(var i=0;i<menuLength;i++){
+    if(menuItem[i].href === currentLocation) {
+        menuItem[i].className = 'active';
+    }
+}
